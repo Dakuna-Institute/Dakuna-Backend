b1780aad80f67a8b231e483e0f7d60341f8cd127
Piyumi Senarath <piyumisenarath12@gmail.com>
2021-07-30 20:37:13 +0530

    Create, Add student and update student details features

---
+++ routes/students.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+let Student = require("../models/Student");
+
+router.route("/add").post((req,res)=>{
+    const name = req.body.name;
+    const age = Number(req.body.age);
+    const gender = req.body.gender;
+
+    const newStudent = new Student({
+        name,
+        age,
+        gender
+    })
+
+    newStudent.save().then(()=>{
+        res.json("Student added")
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+router.route("/").get((req,res)=>{
+    Student.find().then((students)=>{
+        res.json(students)
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
+
+router.route("/update").
+
+module.exports = router;
