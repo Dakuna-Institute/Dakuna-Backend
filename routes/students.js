e57a87a6b9469f74a7989e32e831ce6882dc026b
Piyumi Senarath <piyumisenarath12@gmail.com>
2021-08-05 12:12:37 +0530

    Create backend routes

--- routes/students.js
+++ routes/students.js
@@ -27,6 +27,49 @@ router.route("/").get((req,res)=>{
     })
 })
 
-router.route("/update").
+router.route("/update/:id").put(async(req,res)=>{
+    let userID = req.params.id;
+    const {name,age,gender} = req.body;
+
+    const updateStudent = {
+        name,
+        age,
+        gender
+    }
+
+    const update = await Student.findByIdAndUpdate(userID, updateStudent).then(()=>{
+        res.status(200).send({status: "user updated"});
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).send({status: "error with updating data",error: err.message});
+    })
+
+    
+})
+
+router.route("/delete/:id").delete(async(req,res)=>{
+    let userID = req.params.id;
+    await Student.findByIdAndDelete(userID).then(()=>{
+        res.status(200).send({status: "user deleted"});
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).send({status: "error with delete user",error: err.message});
+    })
+
+    
+})
+
+router.route("/get/:id").get(async(req,res)=>{
+    let userID = req.params.id;
+    const user = await Student.findById(userID).then((student)=>{
+        res.status(200).send({status: "user fetched",student});
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).send({status: "error with fetched user",error: err.message});
+    })
+
+    
+})
+
 
 module.exports = router;
